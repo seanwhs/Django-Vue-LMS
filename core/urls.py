964b940b7f6cfa066cf8a9e57e66366c9fdecf79
unknown <you@example.com>
2026-02-10 08:18:02 +0800

    Initial-Setup Development Environment for Django and View

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('djoser.urls')), # find this urls first
+    path('api/v1/', include('djoser.urls.authtoken')), # find this url if previous did not work
+]
